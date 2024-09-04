6175a2f11a954467e63857e16ac9e66edd9299e9
ImCodeMaker <xxjoshi2006@gmail.com>
2024-09-03 20:20:11 -0400

    added type writer effect

---
+++ src/Components/TypeWriter.jsx
@@ -0,0 +1,33 @@
+// Typewriter.js
+import React, { useState, useEffect } from 'react';
+
+const Typewriter = ({ text, speed, className }) => {
+  const displayText = useTypewriter(text, speed);
+
+  return <p className={className}>{displayText}</p>;
+};
+
+const useTypewriter = (text, speed = 50) => {
+  const [displayText, setDisplayText] = useState('');
+  const [typingIndex, setTypingIndex] = useState(0);
+
+  useEffect(() => {
+    setDisplayText('');
+    setTypingIndex(0);
+  }, [text]);
+
+  useEffect(() => {
+    if (typingIndex < text.length) {
+      const typingInterval = setInterval(() => {
+        setDisplayText(prevText => prevText + text.charAt(typingIndex));
+        setTypingIndex(prevIndex => prevIndex + 1);
+      }, speed);
+
+      return () => clearInterval(typingInterval);
+    }
+  }, [typingIndex, text, speed]);
+
+  return displayText;
+};
+
+export default Typewriter;
--- src/PagesLayout/MainSection.jsx
+++ src/PagesLayout/MainSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '../Components/Button';
+import Typewriter from '../Components/TypeWriter';// Ensure the path is correct
 
 function MainSection() {
   const scrollToServices = () => {
@@ -8,8 +9,12 @@ function MainSection() {
 
   return (
     <div id='home' className='bg-slate-300 min-h-screen flex flex-col justify-center items-center px-4 md:px-8'>
-      <h1 className='text-4xl md:text-6xl lg:text-7xl font-bold font-inter text-center'>
-        Encarnacion Studios.
+      <h1>
+        <Typewriter
+          className='text-4xl md:text-6xl lg:text-7xl font-bold font-inter text-center'
+          text="Encarnacion Studios."
+          speed={100}
+        />
       </h1>
       <div className='mt-6'>
         <Button 
